36a3f4e3c37902816b9d032d84026c7e0b727d4b
MSpiz <reg@panfilova.com>
2024-04-15 20:44:59 +0300

    platform-fix

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,8 @@
-/**
+/
+**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'AqaraIRManager';
+export const PLATFORM_NAME = 'IRManadgerPlatform';
 
 /**
  * This must match the name of your plugin as defined the package.json
